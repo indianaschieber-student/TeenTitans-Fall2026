ff10af92a865a5f4992e3b8887db4b7eb58c1dab
Tristian Santoyo <tristianhugosantoyo@gmail.com>
2026-09-18 19:32:06 -0400

    added a println message which prints my name - Tristian Santoyo

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Teen Titans Team Project");
+        System.out.println("Tristian Santoyo");
     }
 
 }
